eec6dabad614135f511e54ef2f7e4a7dbe2c4119
BlackDot <ksmabcd135@gmail.com>
2021-03-04 23:55:55 +0900

    add button

---
+++ src/components/Reservations/Reservation/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Footer.css';
+
+const footer = () => {
+    return (
+        <div className={classes.Footer}>
+            <button>Join</button>
+            <button>cancel</button>
+        </div>
+    )
+}
+
+export default footer;
--- src/components/Reservations/Reservation/Rerservation.js
+++ src/components/Reservations/Reservation/Rerservation.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './Reservation.css';
 import Title from './Title/Title';
+import Footer from './Footer/Footer';
 
 const reservation = () => {
     return(
         <div className={classes.Item}>
         <Title/>
         test grid layout for Reservations items
+        <Footer/>
         </div>
     )
 }
