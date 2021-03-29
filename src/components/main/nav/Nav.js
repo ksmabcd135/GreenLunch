a74836b1fa671d5af63ba4ce6dbad55447fe74b6
BlackDot <ksmabcd135@gmail.com>
2021-03-30 00:16:49 +0900

    image background setting

--- src/components/main/nav/Nav.js
+++ src/components/main/nav/Nav.js
@@ -13,7 +13,7 @@ class Nav extends React.Component {
             <li><a>MY</a></li>
           </ul>
         </nav>
-        <img className={classes.Food} src={foodImage}/>
+        <header className={classes.Food}/>
       </div>
             
         )
