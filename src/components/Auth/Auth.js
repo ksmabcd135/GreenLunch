1aa15acec6dc2a72ac030a3e8d95c06b21101264
BlackDot <ksmabcd135@gmail.com>
2021-04-07 23:45:58 +0900

    login action component add

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Login from "./Login";
-import Logout from "./Logout";
 import classes from "./Auth.css";
 import logo from "../../img/abo4.svg";
+import Login from "./Login";
+import Logout from "./Logout";
+import { logIn, logOut } from "../../actions";
 
 class Auth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {}
   render() {
     return (
       <div className={classes.Wrapper}>
