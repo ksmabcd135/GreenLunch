54df3bcedbe7ca971453a3b4d562b9a30398c5d9
BlackDot <ksmabcd135@gmail.com>
2021-06-01 00:52:27 +0900

    feat:RoundText component add CSS for RoundText component, adjust props logic for Rountext componet

--- client/src/components/Reservations/Reservation/Rerservation.js
+++ client/src/components/Reservations/Reservation/Rerservation.js
@@ -1,13 +1,12 @@
 import Title from "./Title/Title";
 import Footer from "./Footer/Footer";
 import styles from "./Reservation.module.css";
-
+import RoundText from "../../UI/RoundText";
 const reservation = (props) => {
   return (
     <div className={styles.item}>
       <Title title={props.item.title} />
-      <p>{props.item.text}</p>
-      <p>DATE:{props.item.date}</p>
+      <RoundText title="DATE" text={props.item.date}/>
       <Footer />
     </div>
   );
--- client/src/components/UI/RoundText.js
+++ client/src/components/UI/RoundText.js
@@ -1,7 +1,17 @@
+import classes from './RoundText.module.css';
+import { Fragment } from 'react';
+
 const RoundText = props => {
   return (
-    <div>
-
-    </div>
+    <Fragment>
+      <div className={`${classes['round-text']} ${props.className}`}>
+        {props.title}
+      </div>
+      <div>
+        {props.text}
+      </div>
+    </Fragment>
   );
 }
+
+export default RoundText;
