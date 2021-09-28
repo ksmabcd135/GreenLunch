bbf088594b5c975c5a642fe1657350ed04e41fbd
BlackDot <ksmabcd135@gmail.com>
2021-09-29 01:57:00 +0900

    cors settings(server side)

--- goServer/main.go
+++ goServer/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/ksmabcd135/GreenLunch/controller"
 	"github.com/ksmabcd135/GreenLunch/model"
@@ -8,7 +11,26 @@ import (
 
 func main() {
 	r := gin.Default()
-
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3002",
+		},
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
 	model.ConnectDB()
 
 	r.GET("/schedules", controller.FindSchedules)
